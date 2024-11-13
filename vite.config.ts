2ee92d7199be77477647c2b5314464862bb89a8b
Mateusz Wittek <mati128xd@gmail.com>
2024-11-13 21:40:27 +0100

    initial config finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			// Path aliases matching TypeScript config
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // Path aliases matching TypeScript config
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
