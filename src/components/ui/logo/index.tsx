4f23b19d1ee827654560e1539ad241ffa585d3d4
Mateusz Wittek <mati128xd@gmail.com>
2024-11-14 23:11:04 +0100

    logo typing fix

--- src/components/ui/logo/index.tsx
+++ src/components/ui/logo/index.tsx
@@ -1,15 +1,14 @@
-import { FC } from 'react';
 import { cn } from '@/lib/utils/cn';
 import type { LogoProps } from './types';
 import { styles } from './styles';
 
-export const Logo: FC<LogoProps> = ({
+export const Logo = ({
 	text,
 	className,
 	variant = 'default',
 	asLink = true,
 	href = '/',
-}) => {
+}: LogoProps): JSX.Element => {
 	const Wrapper = asLink ? 'a' : 'div';
 	const baseProps = { role: 'banner' } as const;
 	const linkProps = { ...baseProps, href } as const;
